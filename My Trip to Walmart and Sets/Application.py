744d374d887a56013945dedc3af432e723f473fa
amankhatri <aman_khatri5b5@yahoo.com>
2014-11-29 19:43:36 -0600

    [ ] for list and { } for sets

---
+++ My Trip to Walmart and Sets/Application.py
@@ -0,0 +1,10 @@
+__author__ = 'Aman'
+#set is a collection of items and it can not have duplicate items
+#[] for list {} for sets
+groceries = {"cereal",'milk','starcrunch','beer','ducktape','lotion','beer'}
+print(groceries)
+if 'milk' in groceries:
+    print("You already have milk boss")
+else :
+    print("Oh yea, you need milk")
+

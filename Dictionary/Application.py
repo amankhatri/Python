248986d24c66535edb581baa3aa13f38f80d71d0
amankhatri <aman_khatri5b5@yahoo.com>
2014-11-29 19:45:08 -0600

    key values print(k,v) and for statement for sets

---
+++ Dictionary/Application.py
@@ -0,0 +1,7 @@
+__author__ = 'Aman'
+classmates = {"Tony": "Cool but Smells", "Key" : "Value"} #sequence in sets may not be maintained
+print(classmates)
+print(classmates["Key"])
+print("Loop")
+for k,v in classmates.items():
+    print(k,v)

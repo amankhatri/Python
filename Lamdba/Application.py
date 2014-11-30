4b58b751c01394bbdcc6ab3594eb9c1b01f48d5a
amankhatri <aman_khatri5b5@yahoo.com>
2014-11-29 20:10:35 -0600

    #variableName/FunctionName=lambda input :output cal

---
+++ Lamdba/Application.py
@@ -0,0 +1,10 @@
+__author__ = 'Aman'
+#lambda expression
+#lambda is a small function with no name, that is because they are one time use small functions only
+#creating lambda expression
+#variableName/FunctionName = lambda input : output calculation
+answer = lambda x : x*7
+print(answer(5))
+#we use this :
+# whenever we make a button you would set text of the button to click me and command using lambda expression
+#this action achieves making small function

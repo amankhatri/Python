d8a64f0e781ff64aa55ba2a91f65a1c12750f912
amankhatri <aman_khatri5b5@yahoo.com>
2014-11-29 20:11:32 -0600

    ziping to lists or concatinating ziping is fancy

---
+++ Zip/Applicaion.py
@@ -0,0 +1,6 @@
+__author__ = 'Aman'
+#we will have two list of equal lengths and zip it to gether we will returned a tuple
+first = ['Bucky','Tom','Taylor']
+last = ['Roberts','Hanks','Swift']
+names = zip(first,last)
+#this zip function is making a list of tupes it is doing this  [(bucky, roberts), (Tom, Hank),(Taylor, Swift)]

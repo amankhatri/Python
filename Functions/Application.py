3495d5c20657816055a0a03ac53088bb6f56ec46
amankhatri <aman_khatri5b5@yahoo.com>
2014-11-29 19:24:43 -0600

    Declaring a Function

---
+++ Functions/Application.py
@@ -0,0 +1,10 @@
+__author__ = 'Aman'
+def beef():
+    print("Part of function Line 1")
+    print("Part of function Line 2")
+beef()
+def bitcoin_to_usd(btc):
+    amount = btc * 527
+    print(amount)
+bitcoin_to_usd(3.85)
+
